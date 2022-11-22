e44497d4e91b5e9e19625ac5a00fbf5e471c747d
Ian Gathui <ian.gathui@student.moringaschool.com>
2022-11-22 21:34:33 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+//Test cases 1-2
+const companyName = 'Scuber';
+
+//Test cases 3-4
+let mostProfitableNeighborhood = 'Chelsea';
+
+//Test cases 5-6
+let companyCeo = 'Susan Smith';
